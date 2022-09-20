da777c577154c88fbdd8e4e69fc0c56a39348a7a
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-20 04:52:28 +0100

    Updating Navigation with props
    Adding props to our Navigation

--- App.js
+++ App.js
@@ -3,13 +3,28 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from 'expo-font';
 
-export default function App() {
+//To get our stack
+const Stack = createStackNavigator();
+//creating our custom theme from the default theme
+const theme = {
+  //spread all the elements of the default theme, access the colors, 
+  //spread the defaultTheme.colors, then change the background to transparent
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent"
+  }
+}
+
+const App = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator>
-        <Stack.Screen />
-        <Stack.Screen />
-      </StackNavigator>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
