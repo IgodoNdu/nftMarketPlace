1419f203de243fc087f5b760f4304e37f17dec50
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-20 16:15:23 +0100

    Implement Image display for each NFTCard
    Updating the NFTCard component view with the respective image for each card

--- components/NFTCard.jsx
+++ components/NFTCard.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
-    return (
-      <View>
-        <Text> NFTCard </Text>
-      </View>
-    )
+//import our local assets
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+const NFTCard = ({ data }) => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={{
+      backgroundColor: COLORS.white,
+      borderRadius: SIZES.font,
+      marginBottom: SIZES.extraLarge,
+      margin: SIZES.base,
+      ...SHADOWS.dark
+    }}>
+        <View style={{ width: "100%", height: 250 }}>
+          <Image source={data.image} resizeMode="cover"
+          style={{
+            width: "100%",
+            height: "100%",
+            borderTopLeftRadius: SIZES.font,
+            borderTopRightRadius: SIZES.font,
+          }} />
+        </View>
+    </View>
+  )
 }
 
 export default NFTCard;
