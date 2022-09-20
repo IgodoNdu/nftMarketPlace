b58daa01c757f18337f4b0418020b80281fce1f0
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-20 15:39:53 +0100

    Status bar setup with respect to user focus
    Displaying the status bar if the user focus is true

--- components/FocusedStatusBar.jsx
+++ components/FocusedStatusBar.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
 
-const FocusedStatusBar = () => {
-    return (
-      <View>
-        <Text> FocusedStatusBar </Text>
-      </View>
-    )
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 }
 
 export default FocusedStatusBar;
