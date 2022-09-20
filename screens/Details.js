87c49eb2497687527aa389f25ad9ecd22b411ea2
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-20 12:56:12 +0100

    Component for screens (Home & Details)
    Creating the screens components for home & details (page/screen)

---
+++ screens/Details.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//for text rendering
+import { Text } from 'react-native'
+
+const Details = () => {
+  return (
+    <div>
+      <Text>Details</Text>
+    </div>
+  )
+}
+
+export default Details
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//for text rendering
+import { Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <div>
+      <Text>Home</Text>
+    </div>
+  )
+}
+
+export default Home
