ade8657c665aa94c9d1e778d71ac55b0e39ab274
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-20 15:38:32 +0100

    Basic view setup for NFT display
    Implementing the basic view for the display of NFT data

--- screens/Home.js
+++ screens/Home.js
@@ -11,7 +11,31 @@ import { COLORS, NFTData } from '../constants';
 const Home = () => {
   return (
       <SafeAreaView style={{ flex: 1 }}>
+        <FocusedStatusBar background={COLORS.primary} />
+          
+          <View style={{ flex: 1 }}>
+            {/** View (div) to contain all the NFT list */}
+            <View style={{ zIndex: 0 }}>
+              <FlatList data={NFTData} renderItem={({ item }) => <Text>{item.name}</Text>} 
+              keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={false}
+              ListHeaderComponent={<HomeHeader />} />
+            </View>
 
+            {/** View cover displayed behind the NFT list */}
+            <View style={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              right: 0,
+              left: 0,
+              zIndex: -1,
+            }}>
+              <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+              <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+            </View>
+
+          </View>
       </SafeAreaView>
   )
 }
