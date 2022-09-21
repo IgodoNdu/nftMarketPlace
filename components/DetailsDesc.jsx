819c68551298e1a03bc308242f1654cc84de254c
IgodoNdu <darkbluetechmail@gmail.com>
2022-09-21 01:17:19 +0100

    Updating Subcomponents for the Details screen components
    Updating sub-components for the screen components

---
+++ components/DetailsDesc.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+const DetailsDesc = () => {
+  return (
+    <View>
+      <Text>DetailsBid</Text>
+    </View>
+  )
+}
+
+export default DetailsDesc
--- screens/Details.js
+++ screens/Details.js
@@ -1,10 +1,35 @@
 import React from 'react'
 //for text rendering
-import { Text } from 'react-native'
+import { Text, View, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from '../constants'
+import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components'
 
-const Details = () => {
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
   return (
-      <Text>Details</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <FocusedStatusBar
+          barStyle='dark-content'
+          backgroundColor='transparent'
+          translucent={true} />
+
+      {/**View for self closing button component */}
+      <View style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: 0,
+        paddingVertical: SIZES.font,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(255,255,255,0.5)',
+        zIndex: 1
+      }}>
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+      </SafeAreaView>
+
+      
   )
 }
 
